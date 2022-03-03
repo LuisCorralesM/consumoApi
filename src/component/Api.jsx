83b126ad26e18a88f439b3cd7afbd9a88211f540
Luis Corrales Marin <luiscorrales5012@gmail.com>
2022-03-03 00:17:14 -0500

    imagenes/boton buscar

--- src/component/Api.jsx
+++ src/component/Api.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+// estilos
+import "../styles/styles.css";
+
 export const Api = () => {
   const [data, setData] = useState([]);
 
@@ -11,8 +14,7 @@ export const Api = () => {
 
   const traerData = async (BUSQUEDA) => {
     const API = "https://pokeapi.co/api/v2/pokemon";
-    const apiHero = `${API}/${BUSQUEDA || 'pikachu'}`;
-    // const apiHero = `${API}/pikachu`;
+    const apiHero = `${API}/${BUSQUEDA || "pikachu"}`;
 
     try {
       let res = await axios.get(apiHero);
@@ -23,40 +25,74 @@ export const Api = () => {
 
       setData(dataPokemon);
     } catch (error) {
-      console.log("ups  " + error);
+      console.log("POKEMON NO ENCONTRADO" + error);
+      alert("nombre de pokemon no valido");
+      document.getElementById("input-pokemon").value = "";
     }
   };
 
-  const buscarPokemon = (e)=>{
-    let nombrePokemon = e.target.value.toLowerCase()
+  let nombrePokemon = "";
+
+  const guardarNombre = (e) => {
+    nombrePokemon = e.target.value.toLowerCase();
+  };
+
+  const buscarPokemon = (e) => {
+    e.preventDefault();
+
     console.log(nombrePokemon);
-    traerData(nombrePokemon)
-  }
+    traerData(nombrePokemon);
+  };
 
   console.log(data);
 
   return (
     <>
-      <h1>Consumiendo la API de pokemon</h1>
-
-      <input type="text" placeholder="Nombre del pokemon" className="input-pokemon" onChange={(e)=>buscarPokemon(e)}/>
-      {data ? (
-        data.map((pokemon, i) => (
-          <div key={i}>
-            <h3>datos del pokemon</h3>
-            <ul>
-              <li><span>Nombre del pokemon: </span>{pokemon.name}</li>
-              <li><span>Id: </span>{pokemon.id}</li>
-              <li><span>Order: </span>{pokemon.order}</li>
-            </ul>
+      <h1 className="titulo-principal">Consumiendo la API de pokemon</h1>
+      <div className="contenedor-data">
+        <input
+          type="text"
+          placeholder="Nombre del pokemon"
+          id="input-pokemon"
+          className="input-buscar"
+          onChange={(e) => guardarNombre(e)}
+        />
+        <button 
+        onClick={(e) => buscarPokemon(e)} className="button-buscar">
+          Buscar
+        </button>
+        {data ? (
+          data.map((pokemon, i) => (
+            <div key={i} className='contenedor-data-pokemon'>
+              <h3 className="titulo-datos">Datos del pokemon</h3>
+              <ul>
+                <li className="gird-img">
+                  <img src={pokemon.sprites.front_default} alt=""  className="img-pokemones"/>
+                  <img src={pokemon.sprites.back_default} alt=""  className="img-pokemones"/>
+                  <img src={pokemon.sprites.front_default} alt=""  className="img-pokemones"/>
+                </li>
+                <li>
+                  <span>Nombre del pokemon: </span>
+                  {pokemon.name}
+                </li>
+                <li>
+                  <span>Id: </span>
+                  {pokemon.id}
+                </li>
+                <li>
+                  <span>Order: </span>
+                  {pokemon.order}
+                </li>
+              </ul>
+            </div>
+          ))
+        ) : (
+          <div>
+            {" "}
+            <p>no hay data</p>
           </div>
-        ))
-      ) : (
-        <div>
-          {" "}
-          <p>no hay data</p>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 };
